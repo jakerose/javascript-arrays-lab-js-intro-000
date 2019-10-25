afe8710ca357ac8711dc6a0d921f57ed1ee0c6ee
Jake Rose <jake1020@mac.com>
2019-10-25 22:10:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var slice = kittens.slice(0, kittens.length - 1)
+  var slice = kittens.slice(kittens.length - 1)
   return woof
 }
 
